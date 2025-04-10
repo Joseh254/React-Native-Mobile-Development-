94f4f7922769c651d127fe72032b38bf9a1158c4
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-04-11 02:08:33 +0300

    my first commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >hello joseph</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{
+    display:"flex",
+    flexDirection:"row",
+    alignItems:"center",
+    backgroundColor:"red",
+    width:"100%"
+  }
+})
