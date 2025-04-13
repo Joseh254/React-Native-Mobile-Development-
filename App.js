68f1e65ba113ef735d281bfdc89c202d5c8c1e50
Joseph Mbugua <josephkarimambugua@gmail.com>
2025-04-13 23:51:03 +0300

    add more components

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image,Alert,TouchableWithoutFeedback,TouchableOpacity,Button } from 'react-native';
 
 
 export default function App() {
- 
+  function handlePress(){
+    console.log("you pressed a text");
+   Alert.alert("you pressed a text") 
+  }
   return (
-    <View style={styles.container}>
-      <Text >hello joseph</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title} onPress={handlePress}>Helloo Joseph  mbugua👋</Text>
+        <TouchableOpacity onPress={()=>console.log("you pressed an image")}>
+{/* replace touchables to see the effect on image  */}
+       <Image 
+       blurRadius={1}
+       fadeDuration={2000}
+       source={require("./assets/image.jpeg")}
+       />
+        </TouchableOpacity>
+
+           <Button
+       color={"orange"}
+       title='Click here to alert'
+       onPress={()=>Alert.alert("Title","message goes here",[{text:"yes",onPress:()=>console.log("you pressed yes")
+       },{text:"no",onPress:()=>console.log("You pressed no")
+       }])}
+       />
+      <Text onPress={()=>Alert.prompt("Title here","message here",text=>(console.log(text)
+      ))}>press here to prompt</Text>
+      </View>
+
+   
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    display:"flex",
-    flexDirection:"row",
-    alignItems:"center",
-    backgroundColor:"red",
-    width:"100%"
-  }
-})
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  image: {
+    width: 300,
+    height: 200,
+    borderRadius: 10,
+  },
+});
